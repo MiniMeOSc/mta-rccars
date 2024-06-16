8bca5dc82e145c25d501443ba5878fc015058e41
MiniMeOSc <29428370+MiniMeOSc@users.noreply.github.com>
2024-06-16 13:18:59 +0200

    Added code comments and refactored code

--- client.lua
+++ client.lua
@@ -1,35 +1,44 @@
-local colshape = getElementByID("garage_colshape")
+-- when the player enters the hitbox near the garage door replace its colision to ensure they don't get stuck in it
+local g_colshape = getElementByID("garage_colshape")
 function replaceCollisionModel()
+	-- load the colision model
 	local col = engineLoadCOL("door.col")
-	if col then
-		local replacement = engineReplaceCOL(col, 9823)
-		if not replacement then
-			outputDebugString("The garagedoor collision could not be replaced.", 1)
-		end
-	else
+	if not col then
 		outputDebugString("The garagedoor collision could not be loaded.", 1)
+		return 
+	end
+	
+	-- replace the colision model
+	local replacement = engineReplaceCOL(col, 9823)
+	if not replacement then
+		outputDebugString("The garagedoor collision could not be replaced.", 1)
 	end
 end
-addEventHandler("onClientColShapeHit", colshape, replaceCollisionModel)
+addEventHandler("onClientColShapeHit", g_colshape, replaceCollisionModel)
 
+-- prepare the vehicle selection window
 local spawnWindow
 do
+	-- center the window on the screen
 	local screenWidth, screenHeight = guiGetScreenSize()
 	spawnWindow = guiCreateWindow(screenWidth / 2 - 289 / 2, screenHeight / 2 - 232 / 2, 289, 232, "Remote Controlled vehicles", false)
-	local text = guiCreateLabel(8, 23, 272, 47, "Choose the RC vehicle you want to use.\nEach vehicle has a special weapon type with limited ammunition.", false, spawnWindow)
 	guiWindowSetSizable(spawnWindow, false)
+
+	-- add a little descriptive text
+	local text = guiCreateLabel(8, 23, 272, 47, "Choose the RC vehicle you want to use.\nEach vehicle has a special weapon type with limited ammunition.", false, spawnWindow)
 	guiLabelSetHorizontalAlign(text, "left", true)
+
+	-- create a selection gridlist with 4 columns and fill it
 	local vehicleList = guiCreateGridList(9, 72, 269, 120, false, spawnWindow)
 	guiGridListSetSelectionMode(vehicleList, 0)
-	local listItems =	{
-							{	"Name",		"Type",		"Weapon",		"Ammo",			},
-							{	"Goblin",	"heli",		"aerial bomb",	"1 bomb",		"501",	},
-							{	"Tiger",	"tank",		"tank gun",		"1 shell",		"564",	},
-							{	"Raider",	"heli",		"molotov",		"1 bottle",		"465",	},
-							{	"Bandit",	"car",		"carbomb",		"1 \"BOOM\"",	"441",	},
-							{	"Baron",	"plane",	"machinegun",	"50 bullets",	"464",	},
-							{	"DeLorean",	"car",		"disappear",	"1 use",		"594",	},
-						}
+	local listItems = {
+		{	"Name",		"Type",		"Weapon",		"Ammo",					},
+		{	"Bandit",	"car",		"carbomb",		"1 \"BOOM\"",	"441",	},
+		{	"Baron",	"plane",	"machinegun",	"50 bullets",	"464",	},
+		{	"Raider",	"heli",		"molotov",		"1 bottle",		"465",	},
+		{	"Goblin",	"heli",		"aerial bomb",	"1 bomb",		"501",	},
+		{	"Tiger",	"tank",		"tank gun",		"1 shell",		"564",	},
+	}
 	guiGridListAddColumn(vehicleList, listItems[1][1], 0.255)
 	guiGridListAddColumn(vehicleList, listItems[1][2], 0.15)
 	guiGridListAddColumn(vehicleList, listItems[1][3], 0.3)
@@ -41,37 +50,57 @@ do
 		for column = 1, 4 do
 			guiGridListSetItemText(vehicleList, row, column, listItems[row + 1][column], false, false)
 		end
+		-- store the vehicle ID as item data on the grid list
 		guiGridListSetItemData(vehicleList, row, 1, listItems[row + 1][5])
 	end
+
+	-- add an OK button
 	local confirmButton = guiCreateButton(22, 196, 100, 25, "OK", false, spawnWindow)
 	function processChoice(button, state)
-		if button == "left" and state == "up" then
-			local window = getElementParent(source)
-			local gridlist = getElementsByType("gui-gridlist", window)
-			local row, column = guiGridListGetSelectedItem(gridlist[1])
-			if row ~= -1 and column ~= -1 then
-				triggerServerEvent("onClientRequestsRcVehicle", getLocalPlayer(), tonumber(guiGridListGetItemData(gridlist[1], row, column)))
-				guiSetVisible(getElementParent(source), false)
-				guiSetInputEnabled(false)
-			else
-				outputChatBox("You need to do a selection!")
-			end
+		-- we only want to react to the left mouse button being released
+		if button ~= "left" or state ~= "up" then
+			return
 		end
+
+		-- go up in the element hierarchy and find the gridlist to get the selected row
+		local window = getElementParent(source)
+		local gridlist = getElementsByType("gui-gridlist", window)
+		local row, column = guiGridListGetSelectedItem(gridlist[1])
+
+		-- if no selection has been made abort
+		if row == -1 or column == -1 then
+			return
+		end
+
+		-- relay the selection it to the server and close the window
+		triggerServerEvent("onClientRequestsRcVehicle", getLocalPlayer(), tonumber(guiGridListGetItemData(gridlist[1], row, column)))
+		guiSetVisible(window, false)
+		guiSetInputEnabled(false)
 	end
 	addEventHandler("onClientGUIClick", confirmButton, processChoice)
+
+	-- add a cancel button to close the window
 	local cancelButton = guiCreateButton(167, 196, 100, 25, "Cancel", false, spawnWindow)
 	function closeWindow(button, state)
-		if button == "left" and state == "up" then
-			guiSetVisible(getElementParent(source), false)
-			guiSetInputEnabled(false)
+		-- we only want to react to the left mouse button being released
+		if button ~= "left" or state ~= "up" then
+			return
 		end
+		
+		-- hide the window and disable the mouse
+		guiSetVisible(getElementParent(source), false)
+		guiSetInputEnabled(false)
 	end
 	addEventHandler("onClientGUIClick", cancelButton, closeWindow)
+
+	-- hide the window until the server calls for it
 	guiSetVisible(spawnWindow, false)
 end
 
+-- unhide the window if the server calls for it to be shown
 addEvent("onServerTriggersRcGuiOpen", true)
 function openGui()
+	-- change visibility and enable mouse input
 	guiSetVisible(spawnWindow, true)
 	guiSetInputEnabled(true)
 end
--- server.lua
+++ server.lua
@@ -1,38 +1,41 @@
-local rcVehicles = { [441]=true, [464]=true, [594]=true, [501]=true, [465]=true, [564]=true, }
+------------------------------
+-- Script wide used variables
+------------------------------
 
-local garageDoor = getElementByID("garagedoor")
-setObjectScale(garageDoor, .75)
+-- Define a list of vehicle IDs with the rc vehicles
+local g_rcVehicles = { 
+	[441]=true, -- Bandit
+	[464]=true, -- Baron
+	[465]=true, -- Raider
+	[501]=true, -- Goblin
+	[564]=true  -- Tiger
+}
 
-local colshape = getElementByID("garage_colshape")
+-- get the garage door element from the map
+local g_garageDoor = getElementByID("garagedoor")
+
+-- get the collision shape element for the garage door from the map
+local g_colshape = getElementByID("garage_colshape")
+
+------------------------------
+--	Functionality
+------------------------------
+
+-- recreate the garage collision shape from the map
 do -- enter a new block, makes the following variables only visible and exisiting until we leave it again
-	local ID = getElementID(colshape)
-	local posX, posY, posZ = getElementPosition(colshape)
-	local width = getElementData(colshape, "width")
-	local depth = getElementData(colshape, "depth")
-	local height = getElementData(colshape, "height")
-	local dimension = getElementData(colshape, "dimension")
-	destroyElement(colshape)
-	colshape = createColCuboid(posX, posY, posZ, width, depth, height)
-	setElementDimension(colshape, dimension)
-	setElementID(colshape, ID)
+	local ID = getElementID(g_colshape)
+	local posX, posY, posZ = getElementPosition(g_colshape)
+	local width = getElementData(g_colshape, "width")
+	local depth = getElementData(g_colshape, "depth")
+	local height = getElementData(g_colshape, "height")
+	local dimension = getElementData(g_colshape, "dimension")
+	destroyElement(g_colshape)
+	g_colshape = createColCuboid(posX, posY, posZ, width, depth, height)
+	setElementDimension(g_colshape, dimension)
+	setElementID(g_colshape, ID)
 end
 
-function closeGarageDoor(hitElement, matchingDimension)
-	if getElementType(hitElement) == "player" and matchingDimension and #getElementsWithinColShape(source, "player") < 1 then
-		setGarageDoorOpen(false)
-	end
-end
-addEventHandler("onColShapeLeave", colshape, closeGarageDoor)
-
-function stopGarageDoor(hitElement, matchingDimension)
-	if getElementType(hitElement) == "player" and matchingDimension and #getElementsWithinColShape(source, "player") > 0 then
-		if getElementData(garageDoor, "state") then
-			stopObject(garageDoor)
-		end
-	end
-end
-addEventHandler("onColShapeHit", colshape, stopGarageDoor)
-
+-- recreate the marker from the map
 local marker = getElementByID("marker")
 do -- enter a new block, makes the following variables only visible and exisiting until we leave it again
 	local ID = getElementID(marker)
@@ -47,49 +50,104 @@ do -- enter a new block, makes the following variables only visible and exisitin
 	setElementID(marker, ID)
 end
 
+-- shrink the garage door a little as the model is larger than the actual hole
+setObjectScale(g_garageDoor, .75)
+
+-- when all players leave the hitbox near the garage door, close the door
+function closeGarageDoor(hitElement, matchingDimension)
+	-- if a player had left the hitbox in the same dimension and there are no more other players in the hitbox
+	if getElementType(hitElement) == "player" and matchingDimension and #getElementsWithinColShape(source, "player") < 1 then
+		setGarageDoorOpen(false)
+	end
+end
+addEventHandler("onColShapeLeave", g_colshape, closeGarageDoor)
+
+-- if a player is in the way of the garage door stop its movement
+function stopGarageDoor(hitElement, matchingDimension)
+	-- if there's a player in the same dimension inseide the hitbox
+	if getElementType(hitElement) == "player" and matchingDimension and #getElementsWithinColShape(source, "player") > 0 then
+		-- stop the movement if it's open
+		if getElementData(g_garageDoor, "state") then
+			stopObject(g_garageDoor)
+		end
+	end
+end
+addEventHandler("onColShapeHit", g_colshape, stopGarageDoor)
+
+-- show the vehicle selection ui if the player enters the marker
 function openGui(hitElement, matchingDimension)
+	-- make sure it's the player entering the hitbox and they're in the same dimension
 	if getElementType(hitElement) == "player" and matchingDimension then 
 		triggerClientEvent(hitElement, "onServerTriggersRcGuiOpen", hitElement)
 	end
 end
 addEventHandler("onMarkerHit", marker, openGui)
 
+-- open or close the garage door
 function setGarageDoorOpen(state)
-	local pos = { [true]={x = 189.19999694824, y = 1931.8000488281, z = 19.8}, [false]={x = 189.19999694824, y = 1931.8000488281, z = 17.85}, }
+	-- define the coordinates for the opened and closed position of the gate
+	local pos = { 
+		[true]= {x = 189.19999694824, y = 1931.8000488281, z = 19.8},  -- opened
+		[false]={x = 189.19999694824, y = 1931.8000488281, z = 17.85}, -- closed
+	}
+
+	-- if no state has been given as parameter, get its current state from the element
 	if state == nil then
-		state = getElementData(garageDoor, "state") or false -- "or false" if the state has not yet been set, i.e. it has not yet been opened
+		-- if the state has not yet been set, i.e. it has not yet been opened, assume it's closed
+		state = getElementData(g_garageDoor, "state") or false 
 	end
-	setElementData(garageDoor, "state", not state) -- if it is currently closed, assign it the state value that it is open(ing) ("not state" assigns the opposite of state)
-	local curentPosX, curentPosY, curentPosZ = getElementPosition(garageDoor)
+
+	-- invert the state currently stored on the element
+	setElementData(g_garageDoor, "state", not state) 
+
+	-- move the door. Apply a percentage for the movement it is being interrupted mid movement to normalize the speed
+	-- (i.e. it's still closing but someone is already triggering it again to open. Prevent it from opening too slow)
+	local curentPosX, curentPosY, curentPosZ = getElementPosition(g_garageDoor)
 	local percentage = getDistanceBetweenPoints3D(curentPosX, curentPosY, curentPosZ, pos[state]["x"], pos[state]["y"], pos[state]["z"]) / getDistanceBetweenPoints3D(pos[state]["x"], pos[state]["y"], pos[state]["z"], pos[not state]["x"], pos[not state]["y"], pos[not state]["z"])
-	moveObject(garageDoor, 5000*percentage, pos[state]["x"], pos[state]["y"], pos[state]["z"])
+	moveObject(g_garageDoor, 5000*percentage, pos[state]["x"], pos[state]["y"], pos[state]["z"])
 end
 
+-- spawn an rc car for the player when they click OK on the ui to select one
 addEvent("onClientRequestsRcVehicle", true)
 function spawnRcVehicle(modelID)
+	-- abort if the player is already in rc mode
 	if not exports["RcMode"]:isPlayerInRcMode(playerSource) then
-		local rcVehicle = createVehicle(modelID, 192.48693847656, 1931.5092773438, 17.094734191895, 0, 0, 88.845092773438)
-		setElementDimension(rcVehicle, getElementDimension(source))
-		exports["RcMode"]:enterRcMode(source, rcVehicle)
-		setGarageDoorOpen(true)
+		return
 	end
+	
+	-- create a vehicle, put it in the same dimension as the player and enter rc mode with them
+	local rcVehicle = createVehicle(modelID, 192.48693847656, 1931.5092773438, 17.094734191895, 0, 0, 88.845092773438)
+	setElementDimension(rcVehicle, getElementDimension(source))
+	exports["RcMode"]:enterRcMode(source, rcVehicle)
+
+	-- open the garage door so they can drive out
+	setGarageDoorOpen(true)
 end
 addEventHandler("onClientRequestsRcVehicle", root, spawnRcVehicle)
 
+-- call to the rc mode resource to exit rc mode
 function exitRcMode(keyPresser)
 	local rcVehicle = getPedOccupiedVehicle(keyPresser)
-	if exports["RcMode"]:isPlayerInRcMode(keyPresser) and rcVehicles[getElementModel(rcVehicle)] then
+	-- is the player remote controlling one of our rc models?
+	if exports["RcMode"]:isPlayerInRcMode(keyPresser) and g_rcVehicles[getElementModel(rcVehicle)] then
 		exports["RcMode"]:exitRcMode(keyPresser)
+
+		-- go out with a bang
 		blowVehicle(rcVehicle)
 	end
 end
+
+-- bind key inputs for the player
 function bindKeys(player)
 	bindKey(player, "enter_exit", "down", exitRcMode)
 end
+
+-- handle a player joining the game
 function playerJoinHandler()
 	bindKeys(source)
 end
 addEventHandler("onPlayerJoin", resourceRoot, playerJoinHandler)
+
 function resourceStartHandler()
 	local players = getElementsByType("player")
 	for i = 1, #players, 1 do
@@ -98,6 +156,7 @@ function resourceStartHandler()
 end
 addEventHandler("onResourceStart", resourceRoot, resourceStartHandler)
 
+-- helper function to check whether there's an occupant in the vehicle
 function isVehicleOccupied(vehicle)
 	local occupants = getVehicleOccupants(vehicle)
 	local seats = getVehicleMaxPassengers(vehicle)
@@ -109,30 +168,44 @@ function isVehicleOccupied(vehicle)
 	return false
 end
 
+-- despawn a vehicle and remove map blips and markers
 function despawnVehicle(vehicle)
-	if isElement(vehicle) then
-		if not isVehicleOccupied(vehicle) then
-			local attachedElements = getAttachedElements(vehicle)
-			if attachedElements then
-				for elementKey, elementValue in ipairs(attachedElements) do
-					if isElement(elementValue) then
-						if getElementType(elementValue) == "blip" or getElementType(elementValue) == "marker" then
-							destroyElement(elementValue)
-						end
-					end
+	-- make sure the argument passed is an mta element
+	if not isElement(vehicle) then
+		return
+	end
+
+	-- make sure the vehicle is not occupied
+	if isVehicleOccupied(vehicle) then
+		return
+	end
+
+	-- get any attached elements such as map blips or markers and destroy them
+	local attachedElements = getAttachedElements(vehicle)
+	if attachedElements then
+		for elementKey, elementValue in ipairs(attachedElements) do
+			if isElement(elementValue) then
+				if getElementType(elementValue) == "blip" or getElementType(elementValue) == "marker" then
+					destroyElement(elementValue)
 				end
 			end
-			destroyElement(vehicle)
 		end
 	end
+
+	-- now destroy the vehicle
+	destroyElement(vehicle)
 end
+
+-- create a despawn timer that will despawn the rc cars if they are unsused for more than 60 seconds
 function setDespawnTimer()
 	setTimer(despawnVehicle, 60000, 1, source)
 end
 addEventHandler("onVehicleExplode", resourceRoot, setDespawnTimer)
 
+-- prevent other players from entering an rc car (they're way too small for them anyways)
 function stopEnteringOrExitingRcCar()
-	if rcVehicles[getElementModel(source)] then
+	-- check if the car they want to enter is an rc car
+	if g_rcVehicles[getElementModel(source)] then
 		cancelEvent()
 	end
 end
